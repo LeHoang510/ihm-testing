4beac03dadf65fe880007d318f3934d3c4a92e6b
ThuyTu <vu-thuy-tu.le@insa-rennes.fr>
2023-11-10 09:12:00 +0100

    added switch language

--- frontEnd/front/src/app/header/header.component.ts
+++ frontEnd/front/src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  selectLang:string="";
+  transLang : string[] =[];
+  constructor(public translate: TranslateService){
+    translate.setDefaultLang('fr');
+    translate.addLangs(['en', 'fr']);
+    translate.use('fr');
+    this.selectLang = translate.currentLang;
+  }
 
+  setTransLanguage(){
+  this.translate.use(this.selectLang);
+  }
+  
+  getTransLanguage(){
+    this.transLang=[...this.translate.getLangs()];
+  }
+  
+  ngOnInit(){
+    this.getTransLanguage();
+  }
 }
