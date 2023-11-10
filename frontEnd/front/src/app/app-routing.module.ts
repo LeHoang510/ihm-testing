b318946a65b3a1492cc7910c1894c8841d240a4c
ThuyTu <vu-thuy-tu.le@insa-rennes.fr>
2023-11-10 08:54:58 +0100

    init header

--- frontEnd/front/src/app/app-routing.module.ts
+++ frontEnd/front/src/app/app-routing.module.ts
@@ -1,35 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './components/menu/menu.component';
-import { XmlComponent } from './components/xmlView/xml/xml.component';
-import { ResultComponent } from './components/result/result.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [ //indicate which component to load depending on the path
 
-  {
-    path:"menu",
-    component: MenuComponent
-  },
-  
-  {
-    path:"xmlView",
-    component: XmlComponent
-  },
 
-  {
-    path:"result",
-    component:ResultComponent
-  },
   {
     path: "home",
     component: HomeComponent
   },
   {
-    path: "**",
-    redirectTo: "home",
-    pathMatch: "full"
-  }
+    path:"**",
+    redirectTo:"home"
+  },
+  
 
 
 ];
--- frontEnd/front/src/app/app.module.ts
+++ frontEnd/front/src/app/app.module.ts
@@ -8,15 +8,11 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
 import { TreeTableModule } from 'primeng/treetable';
 import { ButtonModule } from 'primeng/button';
-import { HeaderComponent } from './components/xmlView/header/header.component';
-import { BodyComponent } from './components/xmlView/body/body.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { XmlComponent } from './components/xmlView/xml/xml.component';
 import { InteractoModule } from 'interacto-angular';
-import { ResultComponent } from './components/result/result.component';
 import { HomeComponent } from './home/home.component';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from './header/header.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -26,10 +22,6 @@ export function HttpLoaderFactory(http: HttpClient) {
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent,
-    MenuComponent,
-    XmlComponent,
-    ResultComponent,
     HomeComponent,
   ],
   imports: [
